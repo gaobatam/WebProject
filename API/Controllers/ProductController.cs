88320f4673efeb3ef0fa7fceda6751197b621f23
Loc <ngotanloc141100@gmail.com>
2023-03-25 15:00:52 +0700

    Change Data stucture

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 
+using API.DTOs;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,39 +14,52 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly IProductRepository repo;
+        private readonly IGenericRepository<Product> productRepo;
+        private readonly IGenericRepository<ProductBrand> productBrandRepo;
+        private readonly IGenericRepository<ProductType> productTypeRepo;
+        private readonly IMapper mapper;
+
 
         //private readonly StoreContext context;
 
-        public ProductController(IProductRepository repo)
+        public ProductController(IGenericRepository<Product> productRepo, 
+        IGenericRepository<ProductBrand> productBrandRepo, 
+        IGenericRepository<ProductType> productTypeRepo,
+        IMapper mapper)
         {
-            this.repo = repo;
-            //this.context = context;
-
+            this.productRepo = productRepo;
+            this.productBrandRepo = productBrandRepo;
+            this.productTypeRepo = productTypeRepo;
+            this.mapper = mapper;
         }
         [HttpGet]
-        public async Task <ActionResult<List<Product>>> GetProduct()
+        public async Task <ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProduct()
         { //tách functon này ra thành 1 task riêng, như vậy khi thực hiện query nó sẽ thực thi như là 1 task khác nên không phải đợi khi nó đang thực hiện
-            var products = await repo.GetProductAsync();
-            return Ok(products);
+            
+            var specification= new ProductsWithTypesAndBrandsSpecification();
+            var products = await productRepo.ListAsync(specification);
+            return Ok(mapper
+                .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
         
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
-            return await repo.GetProductByIdAsync(id);
+            var specification = new ProductsWithTypesAndBrandsSpecification(id);
+            var product= await productRepo.GetEntityWithSpec(specification);
+            return mapper.Map<Product,ProductToReturnDto> (product);
         }
 
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
-            return Ok(await repo.GetProductBrandAsync());
+            return Ok(await productBrandRepo.ListAllSync());
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
-            return Ok(await repo.GetProductTypeAsync());
+            return Ok(await productTypeRepo.ListAllSync());
         }
     }
 }
