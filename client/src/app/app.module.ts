fece1856359b75108ce537e1ca887c256528c1b2
Loc <ngotanloc141100@gmail.com>
2023-04-02 02:07:10 +0700

    Polishing a bit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 @NgModule({
   declarations: [
     AppComponent
@@ -22,7 +23,8 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
     HomeModule,
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi:true}
+    {provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi:true},
+    {provide: HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi:true}
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/shop/product-details.component.ts
+++ client/src/app/shop/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { Breadcrumb } from 'xng-breadcrumb/lib/types/breadcrumb';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -10,14 +12,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit{
   product?:Product;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute){}
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService){}
   ngOnInit(): void {
     this.loadProduct();
   }
   loadProduct(){
     const id =this.activatedRoute.snapshot.paramMap.get('id');
     if(id) this.shopService.getProduct(+id).subscribe({
-      next: product=>this.product = product, 
+      next: product=>{
+        this.product = product;
+        this.bcService.set('@productDetails',product.name)
+      }, 
       error: error=> console.log(error)
     })
     
